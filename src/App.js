54da11bb3caf7e9f559dcef306106733787009ae
deepali-23 <sharma.deepali1999@gmail.com>
2023-04-25 13:16:03 +0530

    push

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
-import Dashboard from "./scenes/dashboard";
+import Dashboard from "./scenes/dashboard/index";
 import Sidebar from "./scenes/global/Sidebar";
 // import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
@@ -15,7 +15,7 @@ import Sidebar from "./scenes/global/Sidebar";
 // import Geography from "./scenes/geography";
 // import Calendar from "./scenes/calendar";
 
-function App() {
+export default function App() {
   const [theme, colorMode] = useMode();
   return (
     <ColorModeContext.Provider value={colorMode}>
@@ -27,16 +27,16 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              {/* <Route path="/team" element={<Team />} />
-              <Route path="/geography" element={<Geography />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/faq" element={<FAQ />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/form" element={<Form />} />
-              <Route path="/invoices" element={<Invoices />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/calendar" element={<Calendar />} /> */}
+              {/* <Route path="/team" element={<Team />} /> */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
+              {/* <Route path="/contacts" element={<Contacts />} /> */}
+              {/* <Route path="/pie" element={<Pie />} /> */}
+              {/* <Route path="/faq" element={<FAQ />} /> */}
+              {/* <Route path="/line" element={<Line />} /> */}
+              {/* <Route path="/form" element={<Form />} /> */}
+              {/* <Route path="/invoices" element={<Invoices />} /> */}
+              {/* <Route path="/bar" element={<Bar />} /> */}
+              {/* <Route path="/calendar" element={<Calendar />} />  */}
             </Routes>
           </main>
         </div>
@@ -45,4 +45,4 @@ function App() {
   );
 }
 
-export default App;
+// export default App;
