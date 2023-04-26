9dd8b69731d833312a16774d5beadb76805ab8d8
deepali-23 <sharma.deepali1999@gmail.com>
2023-04-26 13:43:48 +0530

    dashboard

--- src/scenes/dashboard/index.js
+++ src/scenes/dashboard/index.js
@@ -1,3 +1,10 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
 export default function DashBoard() {
-  return <div>Deepali Sharma</div>;
+  return (
+    <Box m="28px">
+      <Box display="flex" justifyContent="space-between" alignItems="center"></Box>
+      <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+    </Box>
+  );
 }
