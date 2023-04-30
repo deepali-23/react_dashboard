28c1d9e0b4bbb345a24f0bfa1b2e5edd0194c595
deepali-23 <sharma.deepali1999@gmail.com>
2023-04-30 15:18:30 +0530

    lime

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 import Bar from "./scenes/bar";
 import Form from "./scenes/form";
-// import Line from "./scenes/line";
+import Line from "./scenes/line";
 import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
@@ -29,11 +29,11 @@ export default function App() {
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
               <Route path="/calendar" element={<Calendar />} />
+              <Route path="/pie" element={<Pie />} />
               {/* <Route path="/geography" element={<Geography />} /> */}
               <Route path="/contacts" element={<Contacts />} />
-              <Route path="/pie" element={<Pie />} />
               <Route path="/faq" element={<FAQ />} />
-              {/* <Route path="/line" element={<Line />} /> */}
+              <Route path="/line" element={<Line />} />
               <Route path="/form" element={<Form />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/bar" element={<Bar />} />
