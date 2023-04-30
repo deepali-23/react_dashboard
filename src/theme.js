e5966472fbee5fe4c00809cd1758ffbebd708862
deepali-23 <sharma.deepali1999@gmail.com>
2023-05-01 00:34:08 +0530

    dashboard integrated

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,7 @@
 import { createContext, useState, useMemo } from "react";
 import { createTheme } from "@mui/material/styles";
 
-//color design tokens
-// ctrlk+ctrl g
+// color design tokens export
 export const tokens = (mode) => ({
   ...(mode === "dark"
     ? {
@@ -78,10 +77,9 @@ export const tokens = (mode) => ({
           100: "#040509",
           200: "#080b12",
           300: "#0c101b",
-          // 400: "#f2f0f0",
-          400: "#1F2A40",
+          400: "#f2f0f0", // manually changed
           500: "#141b2d",
-          600: "#434957",
+          600: "#1F2A40",
           700: "#727681",
           800: "#a1a4ab",
           900: "#d0d1d5",
@@ -121,8 +119,8 @@ export const tokens = (mode) => ({
         },
       }),
 });
-//mui theme settings
 
+// mui theme settings
 export const themeSettings = (mode) => {
   const colors = tokens(mode);
   return {
@@ -130,6 +128,7 @@ export const themeSettings = (mode) => {
       mode: mode,
       ...(mode === "dark"
         ? {
+            // palette values for dark mode
             primary: {
               main: colors.primary[500],
             },
@@ -146,6 +145,7 @@ export const themeSettings = (mode) => {
             },
           }
         : {
+            // palette values for light mode
             primary: {
               main: colors.primary[100],
             },
@@ -162,7 +162,6 @@ export const themeSettings = (mode) => {
             },
           }),
     },
-
     typography: {
       fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
       fontSize: 12,
@@ -194,8 +193,7 @@ export const themeSettings = (mode) => {
   };
 };
 
-//context for color mode
-
+// context for color mode
 export const ColorModeContext = createContext({
   toggleColorMode: () => {},
 });
@@ -211,6 +209,5 @@ export const useMode = () => {
   );
 
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-
   return [theme, colorMode];
 };
