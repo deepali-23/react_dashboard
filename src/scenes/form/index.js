e7fb18eeaecdfec66d01d23b50ae387b2c2afa1f
deepali-23 <sharma.deepali1999@gmail.com>
2023-05-01 15:11:57 +0530

    Formchanges

--- src/scenes/form/index.js
+++ src/scenes/form/index.js
@@ -4,38 +4,24 @@ import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Header from "../../components/Header";
 
-const initialValues = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  contact: "",
-  address1: "",
-  address2: "",
-};
-const phoneRegExp = /^ (?: (?: \+| 0{ 0, 2 }) 91(\s * [\-]\s *) ?| [0] ?)?[789]\d{ 9 } $/;
-const userSchema = yup.object().shape({
-  firstName: yup.string().required("required"),
-  lastName: yup.string().required("required"),
-  email: yup.string().email("invalid email").required("required"),
-  contact: yup.string().matches(phoneRegExp, "Phone number is not valid").required("required"),
-  address1: yup.string().required("required"),
-  address2: yup.string().required("required"),
-});
 const Form = () => {
   const isNonMobile = useMediaQuery("(min-width:600px)");
+
   const handleFormSubmit = (values) => {
     console.log(values);
   };
+
   return (
     <Box m="20px">
       <Header title="CREATE USER" subtitle="Create a New User Profile" />
-      <Formik onSubmit={handleFormSubmit} initialValues={initialValues} validateSchema={userSchema}>
+
+      <Formik onSubmit={handleFormSubmit} initialValues={initialValues} validationSchema={checkoutSchema}>
         {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
           <form onSubmit={handleSubmit}>
             <Box
               display="grid"
               gap="30px"
-              gridTemplateColumns="repeat(4,minmax(0,1fr))"
+              gridTemplateColumns="repeat(4, minmax(0, 1fr))"
               sx={{
                 "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
               }}
@@ -79,7 +65,6 @@ const Form = () => {
                 helperText={touched.email && errors.email}
                 sx={{ gridColumn: "span 4" }}
               />
-
               <TextField
                 fullWidth
                 variant="filled"
@@ -131,4 +116,24 @@ const Form = () => {
     </Box>
   );
 };
+
+const phoneRegExp = /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
+const checkoutSchema = yup.object().shape({
+  firstName: yup.string().required("required"),
+  lastName: yup.string().required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  contact: yup.string().matches(phoneRegExp, "Phone number is not valid").required("required"),
+  address1: yup.string().required("required"),
+  address2: yup.string().required("required"),
+});
+const initialValues = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  contact: "",
+  address1: "",
+  address2: "",
+};
+
 export default Form;
